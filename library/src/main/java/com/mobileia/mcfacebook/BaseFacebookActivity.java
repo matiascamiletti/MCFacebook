2066a456094bd1df6f45a7c1dd57a8517be661d4
Matias Camiletti <matias.camiletti@gmail.com>
2015-07-09 23:32:54 -0300

    Commit initial.

---
+++ library/src/main/java/com/mobileia/mcfacebook/BaseFacebookActivity.java
@@ -0,0 +1,10 @@
+package com.mobileia.mcfacebook;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by matiascamiletti on 9/7/15.
+ */
+public class BaseFacebookActivity extends AppCompatActivity {
+
+}
